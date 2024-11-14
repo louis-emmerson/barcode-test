cc8b41ff44cefa2d8fe5520096b971f5687e8643
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-14 12:29:32 +0000

    basic barcode scanning

--- App.js
+++ App.js
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from "expo-camera"
+import { useState } from "react"
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  SafeAreaViewComponent,
+  TextInput,
+} from "react-native"
 
 export default function App() {
+  const [facing, setFacing] = useState("back")
+  const [permission, requestPermission] = useCameraPermissions()
+  const [cameraActive, setCameraActive] = useState()
+  const [scannedBarcode, setScannedBarcode] = useState()
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    )
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"))
+  }
+
+  if (!cameraActive)
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <TextInput style={styles.input} value={scannedBarcode} />
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setCameraActive(true)
+            }}
+          >
+            <Text style={styles.text}>Start Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView
+        style={styles.camera}
+        facing={facing}
+        onBarcodeScanned={(scannedBarcode) => {
+          console.log(scannedBarcode.data)
+          setScannedBarcode(scannedBarcode.data)
+          setCameraActive(false)
+        }}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
-});
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  barcode: {
+    flex: 1,
+    alignSelf: "flex-start",
+    alignItems: "center",
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "blue",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: "red",
+    fontSize: 30,
+    borderColor: "red",
+  },
+})
